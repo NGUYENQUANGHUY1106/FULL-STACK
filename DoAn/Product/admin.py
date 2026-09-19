8803c5e08b0b1fa4adc365d973aa4f7425f616f8
NGUYENQUANGHUY1106 <nguyenquanghuy110605@gmail.com>
2026-09-19 20:27:54 +0700

    Admin_List_Product

--- DoAn/Product/admin.py
+++ DoAn/Product/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Category,Brand
+from .models import Category,Brand,Cart,Product,history
 admin.site.register(Category)
 admin.site.register(Brand)
+admin.site.register(Cart)
+admin.site.register(history)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    # cấu hình giao diện cho Product
+    search_fields = ['id_user__username']
+    # tìm trang bảng user có name lấy id ra  tương ứng sau đó tìm ở Product có id_user = id  lúc lấy vừa này 
+
+
 
